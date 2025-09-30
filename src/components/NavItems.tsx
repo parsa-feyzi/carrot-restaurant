a5c83cf959c8f291b2751b040b3824642ee247af
Parsa Feyzi <fydyparsa@gmail.com>
2025-09-30 05:57:02 -0700

    create navbar component

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+function NavItems() {
+  const [navs] = useState([
+    { title: "Home", href: "/" },
+    { title: "Menu", href: "/menu" },
+    { title: "Abute us", href: "/abute-us" },
+    { title: "Contact us", href: "/contact-us" },
+  ]);
+
+  const route = usePathname()
+
+  return (
+    <div className="flex items-center justify-center gap-8 text-(--seco)">
+      {navs.map((nav) => (
+        <Link href={nav.href} className={`${ route === nav.href ? "border-b-2 border-(--seco)" :  "hover:border-b-2 duration-50 hover:border-(--seco)"} font-medium`} key={nav.href}>
+          {nav.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default NavItems;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import NavItems from "./NavItems";
+
+function Navbar() {
+  return (
+    <div className="py-6 flex items-center justify-between">
+      <Link href="/">
+        <img className="w-30" src="/icons/icon.png" alt="" />
+      </Link>
+      <NavItems />
+      <div className="flex gap-8 items-center">
+        <Link href="/menu">
+            <img src="/icons/search_icon.png" className="size-4.5 active:scale-95" />
+        </Link>
+        <Link href="/cart">
+            <img src="/icons/basket_icon.png" className="size-5 active:scale-95" />
+        </Link>
+        <div className="py-1.5 px-6 cursor-pointer text-sm rounded-full border border-(--seco) text-(--seco) hover:text-white hover:bg-(--seco) active:scale-95">
+            sign in
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
